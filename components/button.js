14a4427220071bda2943c086e4a93e9d1f3a7364
SPARTAN-175 <avengers_0077@hotmail.com>
2026-07-04 13:39:42 -0600

    Update button.js

--- components/button.js
+++ components/button.js
@@ -1,1 +1,21 @@
+/*
+======================================
+Proyecto : Himnario Cristiano Offline
+Archivo  : button.js
+Versión  : v0.1.0
+Build    : 008
+Autor    : Carlos & ChatGPT
+======================================
+*/
 
+export function crearBoton(texto, id = "") {
+
+    return `
+        <button class="app-button" id="${id}">
+            ${texto}
+        </button>
+    `;
+
+}
+
+// Fin del archivo button.js
