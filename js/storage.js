c800135a75f2594a1f3cbe33d8dd4c076d3a3a54
SPARTAN-175 <avengers_0077@hotmail.com>
2026-07-10 21:20:15 -0600

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -3,17 +3,21 @@
 Proyecto : Himnario Cristiano Offline
 Archivo  : storage.js
 Versión  : v0.2.0
-Build    : 001
+Build    : 004
 Autor    : Carlos & ChatGPT
 ======================================
 */
 
 let cacheHimnos = [];
 
+const CLAVE_FAVORITOS = "hc_favoritos";
+
 export async function obtenerHimnos() {
 
     if (cacheHimnos.length > 0) {
+
         return cacheHimnos;
+
     }
 
     const respuesta = await fetch("./data/himnos.json");
@@ -29,5 +33,43 @@ export async function obtenerHimno(id){
     const himnos = await obtenerHimnos();
 
     return himnos.find(h => h.id === id);
+
+}
+
+export function obtenerFavoritos(){
+
+    const datos = localStorage.getItem(CLAVE_FAVORITOS);
+
+    return datos ? JSON.parse(datos) : [];
+
+}
+
+export function esFavorito(id){
+
+    return obtenerFavoritos().includes(id);
+
+}
+
+export function cambiarFavorito(id){
+
+    let favoritos = obtenerFavoritos();
+
+    if(favoritos.includes(id)){
+
+        favoritos = favoritos.filter(item => item !== id);
+
+    }else{
+
+        favoritos.push(id);
+
+    }
+
+    localStorage.setItem(
+
+        CLAVE_FAVORITOS,
+
+        JSON.stringify(favoritos)
+
+    );
 
 }
