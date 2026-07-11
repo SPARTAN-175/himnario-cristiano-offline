d3162a6f9ee2bf0ef1cafed75cf36f0c40d767e3
SPARTAN-175 <avengers_0077@hotmail.com>
2026-07-10 20:42:04 -0600

    Update router.js

--- js/router.js
+++ js/router.js
@@ -3,15 +3,37 @@
 Proyecto : Himnario Cristiano Offline
 Archivo  : router.js
 Versión  : v0.1.0
-Build    : 004
+Build    : 019
 Autor    : Carlos & ChatGPT
 ======================================
 */
 
 import { mostrarInicio } from "../pages/home.js";
+import { mostrarHimnos } from "../pages/hymns.js";
+
+export function navegar(pantalla) {
+
+    switch (pantalla) {
+
+        case "inicio":
+            mostrarInicio();
+            break;
+
+        case "himnos":
+            mostrarHimnos();
+            break;
+
+        default:
+            mostrarInicio();
+
+    }
+
+}
 
 export function iniciarRouter() {
-    console.log("🧭 Router iniciado");
 
-    mostrarInicio();
+    navegar("inicio");
+
 }
+
+// Fin del archivo
