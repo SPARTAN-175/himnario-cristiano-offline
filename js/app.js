9ef44b0f6cdcbdbdbd1ae2237d62fea2f6043788
SPARTAN-175 <avengers_0077@hotmail.com>
2026-07-13 17:37:53 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,18 +2,28 @@
 ======================================
 Proyecto : Himnario Cristiano Offline
 Archivo  : app.js
-Versión  : v0.1.0
-Build    : 003
+Versión  : v0.3.0
+Build    : 001
 Autor    : Carlos & ChatGPT
 ======================================
 */
 
 import { iniciarRouter } from "./router.js";
 
-function iniciarAplicacion() {
-    console.log("📖 Himnario Cristiano iniciado");
+async function iniciarAplicacion() {
+
+    console.log("📖 Himnario iniciado");
 
     iniciarRouter();
+
+    if ("serviceWorker" in navigator) {
+
+        await navigator.serviceWorker.register("../service-worker.js");
+
+        console.log("✅ Service Worker registrado");
+
+    }
+
 }
 
 iniciarAplicacion();
