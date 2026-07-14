f1d59b48e8cb350c701729b35619f16e92d8f79f
SPARTAN-175 <avengers_0077@hotmail.com>
2026-07-13 18:12:20 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,13 +3,22 @@
 Proyecto : Himnario Cristiano Offline
 Archivo  : app.js
 Versión  : v0.3.0
-Build    : 013
+Build    : 015
 Autor    : Carlos & ChatGPT
 ======================================
 */
 
 import { iniciarRouter } from "./router.js";
+
 import { abrirBaseDatos } from "./database.js";
+
+import {
+
+    contarHimnos,
+
+    guardarHimnos
+
+} from "./database-hymns.js";
 
 async function iniciarAplicacion() {
 
@@ -17,13 +26,27 @@ async function iniciarAplicacion() {
 
     await abrirBaseDatos();
 
+    const cantidad = await contarHimnos();
+
+    if (cantidad === 0) {
+
+        console.log("📥 Primera ejecución...");
+
+        const respuesta = await fetch("./data/himnos.json");
+
+        const himnos = await respuesta.json();
+
+        await guardarHimnos(himnos);
+
+        console.log("✅ Himnos cargados en IndexedDB");
+
+    }
+
     iniciarRouter();
 
     if ("serviceWorker" in navigator) {
 
         await navigator.serviceWorker.register("./service-worker.js");
-
-        console.log("✅ Service Worker registrado");
 
     }
 
